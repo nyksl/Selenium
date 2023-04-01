5a44ab5f3d184d06a5f447128dd4dae97e76dcb3
Nusret Yuksel <trsnykl@gmail.com>
2023-04-01 21:56:40 +0300

    My Selenium

---
+++ src/test/java/day01_DriverMethots/ilkclas.java
@@ -0,0 +1,16 @@
+package day01_DriverMethots;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ilkclas {
+
+    public static void main(String[] args) {
+
+       // System.setProperty("webdriver.chrome.driver","src/resources/drivers/chromedriver.exe");
+        System.setProperty("webdriver.http.factory", "jdk-http-client");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.techproeducation.com");
+    }
+}
